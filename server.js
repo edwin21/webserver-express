586cff33cdb275aee24d95e054db6e723d22a7c0
Edwin <edwin21hernandez@gmail.com>
2020-07-17 18:00:22 -0700

    primer commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+
+
+require('./hbs/helpers/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials', function(err) {});
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    // res.send('Hola mundo');
+
+    res.render('home', {
+        nombre: 'EDwIn'
+
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        nombre: 'Edwin'
+
+    });
+});
+
+
+app.get('/data', (req, res) => {
+    res.send('Hola data');
+
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
